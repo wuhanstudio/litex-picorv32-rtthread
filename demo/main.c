470e8eea68f84aeab3a3401a5c8df880ee4b649e
wuhanstudio <wuhanstudio@qq.com>
2023-09-26 21:15:11 +0100

    Add ISR

--- demo/main.c
+++ demo/main.c
@@ -1,9 +1,11 @@
 #include <irq.h>
 #include <stdio.h>
+#include <libbase/uart.h>
 
-#define reg_uart_data (*(volatile uint32_t*)CSR_UART_BASE)
+#define OUTPORT (*(volatile uint32_t*)CSR_UART_BASE)
 
 void myprintf(const char *str);
+void myirq(void);
 
 void myprintf(const char *str)
 {
@@ -12,16 +14,60 @@ void myprintf(const char *str)
     {
         if(str[i] == '\n')
         {
-            reg_uart_data = '\r';
+            OUTPORT = '\r';
         }
-        reg_uart_data = str[i];
+        OUTPORT = str[i];
     }
 }
 
+int volatile timeup = 0;
+
 int main(void)
 {
+	irq_setmask(0);
+	irq_setie(1);
+
+	uart_init();
+
+    timer0_ev_enable_write(1);
+	irq_setmask(irq_getmask() | (1 << TIMER0_INTERRUPT));
+
     printf("Hello World (stdc)\n");
-    myprintf("Hello World (serial)\n");
+
+    timer0_en_write(0);
+    timer0_load_write(0);
+    timer0_reload_write(CONFIG_CLOCK_FREQUENCY);
+    timer0_en_write(1);
+
+    while(1)
+    {
+        if(timeup) {
+            myprintf("Hello World (serial)\n");
+            timeup = 0;
+        }
+    };
 
     return 0;
+}
+
+void myirq(void)
+{
+	__attribute__((unused)) unsigned int irqs;
+
+	irqs = irq_pending() & irq_getmask();
+
+	if(irqs & (1 << TIMER0_INTERRUPT))
+    {
+    	printf("TIMER0 Interrupt \n");
+        timer0_ev_pending_zero_write(1);
+        timeup = 1;
+    }
+
+#ifdef CSR_UART_BASE
+#ifndef UART_POLLING
+	if(irqs & (1 << UART_INTERRUPT))
+    	printf("UART Interrupt \n");
+		uart_isr();
+#endif
+#endif
 }
--- demo/picorv32/crt0.d
+++
@@ -1,6 +0,0 @@
-picorv32/crt0.o: picorv32/crt0.S \
- /usr/riscv64-linux-gnu/include/stdc-predef.h picorv32/custom_ops.S
-
-/usr/riscv64-linux-gnu/include/stdc-predef.h:
-
-picorv32/custom_ops.S:
