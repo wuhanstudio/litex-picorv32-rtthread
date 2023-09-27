b87f51552612a9fa170df47e86360025dbe5fd57
wuhanstudio <wuhanstudio@qq.com>
2023-09-27 15:31:16 +0100

    Use LiteX ISR

--- demo/main.c
+++ demo/main.c
@@ -2,25 +2,11 @@
 #include <stdio.h>
 #include <libbase/uart.h>
 
-#define OUTPORT (*(volatile uint32_t*)CSR_UART_BASE)
-
-void myprintf(const char *str);
+// Interrupt handler
 void myirq(void);
 
-void myprintf(const char *str)
-{
-    int i=0;
-    for(i=0;'\0' != str[i];i++)
-    {
-        if(str[i] == '\n')
-        {
-            OUTPORT = '\r';
-        }
-        OUTPORT = str[i];
-    }
-}
-
 int volatile timeup = 0;
+int volatile count = 0;
 
 int main(void)
 {
@@ -32,8 +18,6 @@ int main(void)
     timer0_ev_enable_write(1);
 	irq_setmask(irq_getmask() | (1 << TIMER0_INTERRUPT));
 
-    printf("Hello World (stdc)\n");
-
     timer0_en_write(0);
     timer0_load_write(0);
     timer0_reload_write(CONFIG_CLOCK_FREQUENCY);
@@ -42,7 +26,7 @@ int main(void)
     while(1)
     {
         if(timeup) {
-            myprintf("Hello World (serial)\n");
+            printf("Hello World %d\n", count);
             timeup = 0;
         }
     };
@@ -53,14 +37,14 @@ int main(void)
 void myirq(void)
 {
 	__attribute__((unused)) unsigned int irqs;
-
 	irqs = irq_pending() & irq_getmask();
 
 	if(irqs & (1 << TIMER0_INTERRUPT))
     {
-    	printf("TIMER0 Interrupt \n");
         timer0_ev_pending_zero_write(1);
+
         timeup = 1;
+        count++;
     }
 
 #ifdef CSR_UART_BASE
@@ -70,4 +54,5 @@ void myirq(void)
 		uart_isr();
 #endif
 #endif
+    return;
 }
