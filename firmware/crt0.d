113e0a6048696ea7666e71c8c934799c78548269
wuhanstudio <wuhanstudio@qq.com>
2023-09-30 16:07:05 +0100

    Remove rust and bios

--- firmware/crt0.d
+++
@@ -1,8 +0,0 @@
-crt0.o: \
- /home/wuhanstudio/python-litex/litex/litex/soc/cores/cpu/picorv32/crt0.S \
- /usr/riscv64-linux-gnu/include/stdc-predef.h \
- /home/wuhanstudio/python-litex/litex/litex/soc/cores/cpu/picorv32/extraops.S
-
-/usr/riscv64-linux-gnu/include/stdc-predef.h:
-
-/home/wuhanstudio/python-litex/litex/litex/soc/cores/cpu/picorv32/extraops.S:
